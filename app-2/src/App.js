a211985a91a41be52539feebeb4faf8ae9a1e3a8
Yenalgloshi <jasonbegay@msn.com>
2018-03-28 14:02:02 -0600

    reset code

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,25 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(){
-    super();
-
-    this.state={
-      foods: [
-        "pizza",
-        "burgers"
-      ]
-    }
-  }
   render() {
-    let foodsToDisplay = this.state.foods.map((e,i) => {
-      return (
-        <h2 key={i}>{e}</h2>
-      )
-    })
     return (
       <div className="App">
-        { foodsToDisplay }
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
