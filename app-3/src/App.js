f6af9ae8fbf5a86c536cc6d2597873bbfa8e52d7
Yenalgloshi <jasonbegay@msn.com>
2018-04-24 09:54:00 -0600

    practice push completed to #7

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,20 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
+class App extends Component{
+  constructor(){
+    super()
+
+    this.state = {
+      filterStrings:"",
+      foods: ['milk', 'eggs', 'bread', 'cheese', 'butter', 'potatoes', 'meat']
+    }
+  }
+
+  handleChange(filter){
+    this.setState ({filterStrings: filter})
+  }
+
+  render(){
+    let foodsToDisplay = this.state.foods.filter((e,i) => {
+      return e.includes(this.state.filterStrings)
+    }).map((e,i) => {
+      return <h2 key={i}>{e}</h2>
+    })
+    return(
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={ (e) => this.handleChange(e.target.value)} type="text"/>
+        {foodsToDisplay}
       </div>
-    );
+    )
   }
 }
 
--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,20 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Todo from './Todo';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+class App extends Component{
+  constructor(){
+    super()
+
+    this.state = {
+      list: [],
+      input: ''
+    }
+    this.handleAddTask = this.handleAddTask.bind(this);
+  }
+
+handleInputChange(value){
+  this.setState({input: value})
+}
+
+handleAddTask(){
+  this.setState({list: [...this.state.list, this.state.input], input:''})
+}
+
+  render(){
+    let list = this.state.list.map((e, i) => {
+      return(
+        <Todo key={i} task={e}/>
+      )
+    })
+    return(
+      <div className='App'>
+        <h1>My to-do list:</h1>
+        <div>
+          <input value={this.state.input}
+                 placeholder='Enter New Task'
+                 onChange={(e) => this.handleInputChange(e.target.value)} />
+          <button onClick={this.handleAddTask}>Add</button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br/>
+        {list}
       </div>
-    );
+    )
+
   }
 }
 
